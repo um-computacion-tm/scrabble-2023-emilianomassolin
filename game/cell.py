0794a9391617ea8e62895cee41bffeac1248e614
emilianomassolin <e.massolin@alumno.um.edu.ar>
2023-09-07 00:22:25 -0300

    add the cell deactivation function

--- game/cell.py
+++ game/cell.py
@@ -14,7 +14,7 @@ class Cell:
     def calculate_value(self):
         if self.letter is None:
             return 0
-        if self.multiplier_type == 'letter':
+        if self.multiplier_type == 'letter'and self.state==True:
             return self.letter.value * self.multiplier
         else:
             return self.letter.value
@@ -26,7 +26,12 @@ class calculate_word:
         for letras in self.word:
             value_letra=Cell.calculate_value(letras)
             value+=value_letra
+        for letras in self.word:
+            if letras.multiplier_type == 'word' and letras.state == True:
+                value = value * letras.multiplier
         return value    
-            
+    
+        
+              
         
         
