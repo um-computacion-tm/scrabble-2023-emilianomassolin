0936e89ad9793b093086d1565cdbe60ccfeb1d5f
emilianomassolin <e.massolin@alumno.um.edu.ar>
2023-09-12 11:33:54 -0300

    add function validate_word_inside_board

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,3 +11,5 @@ Add the word and letter multiplier tests.
 modify the cell class, add a " calculate_word " class inside the cell folder, put a function to calculate the value of the word inside the calculate_word class and add the code climate.
 2023-09-07
 fixing git ignore issues,add new features to the scrable folder, the function of turns with their respective tests.
+2023-09-12
+We create the function validate_word_inside_board to know the location of the word on the board.
--- tests/test_board.py
+++ tests/test_board.py
@@ -12,6 +12,25 @@ class TestBoard(unittest.TestCase):
             len(board.grid[0]),
             15,
         )
+    def test_word_inside_board(self):
+         board= Board()
+         word = "Facultad"
+         location = (5, 4)
+         orientation = "H"
+
+         word_is_valid = board.validate_word_inside_board(word, location, orientation)
+
+         assert word_is_valid == True
+    
+
+    def test_word_out_of_board(self):
+        board = Board()
+        word = "Facultad"
+        location = (14, 4)
+        orientation = "H"
+        word_is_valid = board.validate_word_inside_board(word, location, orientation)
+
+        assert word_is_valid == False
 
 if __name__ == '__main__':
     unittest.main()
